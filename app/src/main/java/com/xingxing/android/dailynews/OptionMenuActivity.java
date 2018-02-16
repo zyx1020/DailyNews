05ce56aba214ddb565a7da04328e8f425921e277
Joanna Zhou <zyx_931020@163.com>
2018-02-15 23:51:52 -0800

    delete redundant buttons to prepare for menu

---
+++ app/src/main/java/com/xingxing/android/dailynews/OptionMenuActivity.java
@@ -0,0 +1,16 @@
+package com.xingxing.android.dailynews;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ZHOU on 2/15/18.
+ */
+
+public class MenuActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+}
